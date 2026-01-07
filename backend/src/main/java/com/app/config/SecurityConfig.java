a0e9b22cc37b0e3e77e93d92fab99f04c1046101
khde28 <emamaniyu@unsa.edu.pe>
2026-01-07 07:13:47 -0500

    Configurar sesión STATELESS y agregar filtro JWT #25

--- backend/src/main/java/com/app/config/SecurityConfig.java
+++ backend/src/main/java/com/app/config/SecurityConfig.java
@@ -14,7 +14,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
+import com.app.config.filter.JwtTokenValidatorFilter;
+import com.app.resources.JwtUtil;
 import com.app.services.implementations.UserService;
 
 @Configuration
@@ -22,35 +25,39 @@ import com.app.services.implementations.UserService;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-  public SecurityConfig() {
+  private JwtUtil jwtUtil;
+
+  public SecurityConfig (JwtUtil jwtUtil) {
+    this.jwtUtil = jwtUtil;
   }
 
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+  public SecurityFilterChain securityFilterChain (HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
-        .csrf(csrf -> csrf.disable())
+        .csrf(csrf -> csrf.disable() )
         .httpBasic(Customizer.withDefaults())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED))
+        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .addFilterBefore(new JwtTokenValidatorFilter(jwtUtil),BasicAuthenticationFilter.class)
         .build();
   }
 
   @Bean
-  public AuthenticationProvider authenticationProvider(UserService userServiceImp) {
+  public AuthenticationProvider authenticationProvider(UserService userServiceImp){
     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
     provider.setPasswordEncoder(passwordEncoder());
     provider.setUserDetailsService(userServiceImp);
 
     return provider;
+
   }
 
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-      throws Exception {
+  public AuthenticationManager authenticationManager (AuthenticationConfiguration authenticationConfiguration) throws Exception {
     return authenticationConfiguration.getAuthenticationManager();
   }
 
   @Bean
-  public PasswordEncoder passwordEncoder() {
+  public PasswordEncoder passwordEncoder () {
     return new BCryptPasswordEncoder();
   }
 

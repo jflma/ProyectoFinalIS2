470f5988d74f55916f35460c14211d304d91aba4
khde28 <emamaniyu@unsa.edu.pe>
2026-01-07 21:36:26 -0500

    agregar pruebas unitarias para funcionalidad de login con JWT #25

--- backend/src/test/java/com/app/services/implementations/UserServiceTest.java
+++ backend/src/test/java/com/app/services/implementations/UserServiceTest.java
@@ -1,5 +1,4 @@
 package com.app.services.implementations;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -21,11 +20,14 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import com.app.controller.dto.LoginRequestDTO;
 import com.app.controller.dto.SignupFieldsDTO;
+import com.app.controller.dto.response.TokenResponse;
 import com.app.domain.user.ForoUser;
 import com.app.domain.user.Person;
 import com.app.domain.user.Role;
 import com.app.repositories.UserRepositoryImp;
+import com.app.resources.JwtUtil;
 import com.app.services.interfaces.IPersonService;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,12 +39,14 @@ class UserServiceTest {
   private UserRepositoryImp userRepository;
   @Mock
   private PasswordEncoder passwordEncoder;
+  @Mock
+  private JwtUtil jwtUtil;
 
   @InjectMocks
   private UserService userService;
 
   @Test
-  void testRegisterUser() {
+  void testRegisterUser () {
     LocalDate birthday = LocalDate.now();
     String firstName = "nombre1";
     String lastName = "nombre2";
@@ -53,49 +57,51 @@ class UserServiceTest {
     Person person = new Person();
     person.setEmail(email);
 
-    SignupFieldsDTO fields = new SignupFieldsDTO(firstName, lastName, email, birthday, userName, password);
+
+    
+    SignupFieldsDTO fields = new SignupFieldsDTO(firstName,lastName,email,birthday,userName,password);
 
     ForoUser user = new ForoUser();
     user.setUsername(userName);
     user.setPerson(person);
 
-    // when
-    when(personService.createPerson(firstName, lastName, email, birthday)).thenReturn(person);
+    //when
+    when(personService.createPerson(firstName,lastName,email,birthday)).thenReturn(person);
     when(userRepository.save(any(ForoUser.class))).thenReturn(user);
 
-    // act
+    //act
     ForoUser userCreated = userService.registerUser(fields);
 
     // assert
     assertNotNull(userCreated);
     assertEquals(user.getUsername(), userCreated.getUsername());
-
+    
   }
 
   @Test
-  void testGetUserbyId() {
+  void testGetUserbyId () {
     Long id = 1L;
     ForoUser user = new ForoUser();
     user.setId(id);
 
-    // when
+    //when 
     when(userRepository.findById(id)).thenReturn(Optional.of(user));
 
-    // act
+    //act
     ForoUser userFound = userService.getUserbyId(id);
 
-    // assert
+    //assert 
     assertNotNull(userFound);
     assertEquals(id, userFound.getId());
   }
 
   @Test
-  void testGetUserByUsername() {
+  void testGetUserByUsername () {
     String username = "username";
     ForoUser user = new ForoUser();
     user.setUsername(username);
 
-    // when
+    //when 
     when(userRepository.findForoUserByUsername(username)).thenReturn(Optional.of(user));
 
     ForoUser userFound = userService.getUserByUsername(username);
@@ -105,7 +111,7 @@ class UserServiceTest {
   }
 
   @Test
-  void testLoadUserByUsername() {
+  void testLoadUserByUsername () {
     String username = "test1";
     String password = "password";
 
@@ -125,7 +131,7 @@ class UserServiceTest {
   }
 
   @Test
-  void testAuthenticate() {
+  void testAuthenticate () {
     String username = "test1";
     String password = "password";
     String role = "ADMIN";
@@ -137,9 +143,10 @@ class UserServiceTest {
     user.setPassword(password);
     user.setRoles(Set.of(admin));
 
+
     List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
     authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(role)));
-
+    
     when(userRepository.findForoUserByUsername(username)).thenReturn(Optional.of(user));
     when(passwordEncoder.matches(password, password)).thenReturn(true);
 
@@ -147,5 +154,30 @@ class UserServiceTest {
 
     assertNotNull(userSucessfully);
 
+  }
+
+  @Test
+  void testLoginUser () {
+    String username = "test1";
+    String password = "password";
+    String token = "token";
+
+    ForoUser user = new ForoUser();
+    Role admin = new Role();
+    admin.setName("ADMIN");
+    user.setUsername(username);
+    user.setPassword(password);
+    user.setRoles(Set.of(admin));
+
+    LoginRequestDTO fields = new LoginRequestDTO(username, password);
+
+    when(userRepository.findForoUserByUsername(username)).thenReturn(Optional.of(user));
+    when(passwordEncoder.matches(password, password)).thenReturn(true);
+    when(jwtUtil.generateToken(any(Authentication.class))).thenReturn(token);
+
+    TokenResponse jwt = userService.loginUser(fields);
+
+    assertNotNull(jwt);
+    assertEquals(token, jwt.getToken());
   }
 }

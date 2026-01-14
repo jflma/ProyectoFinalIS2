dc149ca4a593734c62966bdf5cecdb42d18c6d37
jflma <145713315+jflma@users.noreply.github.com>
2026-01-13 21:15:49 -0500

    fix(persistence): corrección de rutas de paquetes en consultas JPQL de repositorios Personalizados

--- backend/src/main/java/com/app/modules/post/persistence/PostRepositoryImp.java
+++ backend/src/main/java/com/app/modules/post/persistence/PostRepositoryImp.java
@@ -13,11 +13,11 @@ import com.app.modules.post.domain.Post;
 @Repository
 public interface PostRepositoryImp extends JpaRepository<Post, Long> {
 
-  @Query("SELECT new com.app.controller.dto.response.PostPreviewDTO(p.id, p.title, p.views, p.answers, e.content, e.upVotes, e.downVotes)"
+  @Query("SELECT new com.app.modules.post.controller.dto.response.PostPreviewDTO(p.id, p.title, p.views, p.answers, e.content, e.upVotes, e.downVotes)"
       + "FROM Post p JOIN p.entry e ORDER BY p.entry.createdAt DESC")
   public List<PostPreviewDTO> findTop10ByOrderByEntryCreatedAt();
 
-  @Query("SELECT new com.app.controller.dto.response.PostDetailsDTO(p.id, e.id, p.title, e.content, e.createdAt, u.username)"
+  @Query("SELECT new com.app.modules.post.controller.dto.response.PostDetailsDTO(p.id, e.id, p.title, e.content, e.createdAt, u.username)"
       + "FROM Post p JOIN p.entry e JOIN  e.user u  WHERE p.id= :idPost")
   public PostDetailsDTO findPostById(Long idPost);
 
